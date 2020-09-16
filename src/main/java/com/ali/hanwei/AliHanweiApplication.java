2e3623a9bd967f2ab3dbd8af33cad4df55b30580
“ <865169211@qq.com>
2020-09-16 09:57:52 +0800

    ali-hanwei project init

---
+++ src/main/java/com/ali/hanwei/AliHanweiApplication.java
@@ -0,0 +1,13 @@
+package com.ali.hanwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliHanweiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliHanweiApplication.class, args);
+    }
+
+}
